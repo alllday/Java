b8923187d5fadca68bbe6fcd2f64e0e2d8885c12
alllday <124110632+alllday@users.noreply.github.com>
2023-03-09 21:45:53 +0900

    Add files via upload

---
+++ jbdc01/src/자바DB연결/ProductDAO.java
@@ -0,0 +1,12 @@
+package 자바DB연결;
+
+import 화면DB연결.ProductVO;
+
+public class ProductDAO {
+	public void insert(ProductVO bag) {
+		System.out.println("전달된 id는 " + bag.getId());
+		System.out.println("전달된 name는 " + bag.getName());
+		System.out.println("전달된 content는 " + bag.getContent());
+		
+	}
+}
